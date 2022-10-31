71f153014e0cb11cbe32811b8dc0e82dec6071ea
MoisesFontana <moisesfontana@gmail.com>
2022-10-31 13:34:45 -0300

    Alterado o tamanho da imagem ss-1.png e alterado aquivo js para formatar a saida do campo Weigth do card de detalhes

--- assets/js/main-detail.js
+++ assets/js/main-detail.js
@@ -1,6 +1,6 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const pokeId = urlParams.get('pokeId')
+const pokeId = urlParams.get('pokeId');
 
 //-- Código baseado no projeto encontrado em
 //-- https://github.com/Siphiwo/pokemon-codex-search
@@ -14,11 +14,30 @@ const getPokemonData = async term => {
 
     const pokemon = await response.json()
 
-    // update ui with data 
+    //-- Conversão Kg(weight) em decimal
+    var convertoToArray = String(pokemon.weight).split("").map((kg)=>{
+        return Number(kg);  
+    })
+
+    let peso = 0;
+    let i = 0;
+    if (convertoToArray.length <= 3) {
+        peso = convertoToArray[0] + '.';
+        i = 1;
+    } else {
+        peso = convertoToArray[0];
+        peso += convertoToArray[1] + '.';
+        i = 2;
+    }
+    for ( i; i < convertoToArray.length; i++) {
+        peso += convertoToArray[i];
+    }
+
+    // Atualiza os elementos HTML com os dados do Fetch()
     document.getElementById('poke-id').innerHTML = `#${pokemon.id}`
     document.getElementById('poke-img').setAttribute('src', pokemon.sprites.other.dream_world.front_default)
     document.getElementById('poke-name').innerHTML = pokemon.name
-    document.getElementById('poke-weight').innerHTML = `${pokemon.weight}kg`
+    document.getElementById('poke-weight').innerHTML = `${peso}kg`
     document.getElementById('poke-height').innerHTML = `0.${pokemon.height}m`
     document.getElementById('poke-type').innerHTML = `<span class="${pokemon.types[0].type.name}">${pokemon.types[0].type.name}</span>`
     document.getElementById('container').classList.add(pokemon.types[0].type.name)
@@ -26,7 +45,6 @@ const getPokemonData = async term => {
     document.getElementById('poke-ability').innerHTML = ` ${pokemon.abilities[0].ability.name}`
     document.getElementById('poke-ability').innerHTML += `, ${pokemon.abilities[1].ability.name}`
     document.getElementById('poke-type').innerHTML += `<span class="${pokemon.types[1].type.name}">${pokemon.types[1].type.name}</span>`
-
 }
 
 getPokemonData(pokeId)
